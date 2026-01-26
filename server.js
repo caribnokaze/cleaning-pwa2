71a3ab50953051b5465fa63d3d31e59f77b32196
caribnokaze <125463186+caribnokaze@users.noreply.github.com>
2026-01-26 17:09:39 +0900

    Update GAS_URL to new Google Apps Script endpoint

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' })); // 画像データ用に上限を拡張
 app.use(express.static('.')); // index.htmlなどを表示する設定
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbxeFIBXnIu6if5OovTpFLSX-DjZNVtw_r7FnWFH-TdEupns2Yd81f455klY4sBCNkemsw/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxNnsmFUj3HS9wqdKgFNYoWMMF5AFcyr1UOc2CVomWyuJy4LccIFtA8RBV7IDCdciWj0w/exec";
 
 // 画像送信を受け付ける窓口
 app.post('/upload', (req, res) => {
