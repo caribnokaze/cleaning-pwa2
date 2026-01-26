59a0ac860d341cabdc7a47e6d8d880e904ef84aa
caribnokaze <125463186+caribnokaze@users.noreply.github.com>
2026-01-26 23:11:42 +0900

    Update GAS_URL to new endpoint

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' })); // 画像データ用に上限を拡張
 app.use(express.static('.')); // index.htmlなどを表示する設定
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbyFMdjP_vUhWk1-usMhO2OGDvw_rAsNu-ZWWKBcXrSTfDUFlm0Ij3p8Dab7Xj5GnSWzvA/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxWuudDJ2--PKL5tRLHF_i-nFB19AKd32OSHLkpnr73lT_a6otvJUfbakRjnuIGOCuOdQ/exec";
 
 // 画像送信を受け付ける窓口
 app.post('/upload', (req, res) => {
