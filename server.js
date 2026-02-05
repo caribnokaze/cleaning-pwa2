b85148d9444bd79f8c29bf3ac18d0a81d9e8f192
caribnokaze <125463186+caribnokaze@users.noreply.github.com>
2026-02-05 14:30:21 +0900

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(express.static('.'));
 
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwXrxg-1sFxNQ-FHgN031XNyLadHD3DRrX1rOb2ooXpdfp-r_Sm8rCypkNF1AaYqdxKFQ/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxwG5f6julEHVKgXA5sTRTKj-ZUKwvpUsqa75jAQwZjqBPpOrsI7rk5M3RpFCnc7YK5ig/exec";
 
 // 画像送信を受け付ける窓口
 app.post('/upload', (req, res) => {
